2f24c155acac2d2a6de50ad92d2f6684ea277852
vijay-rajak <vijaybca2022@gmail.com>
2023-01-30 10:44:39 +0530

    first commit

---
+++ student_management_project/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,redirect,HttpResponse
+from app.EmailBackEnd import EmailBackEnd
+from django.contrib.auth import authenticate,logout,login
+from django.contrib import messages
+
+def BASE(request):
+    return render(request,'base.html')
+
+
+def LOGIN(request):
+    return render(request,'login.html')
+
+def doLogin(request):
+    if request.method == 'POST':
+        user = EmailBackEnd.authenticate(request,
+                                         username=request.POST.get('email'),
+                                         password=request.POST.get('password'),)
+        if user!=None:
+            login(request,user)
+            user_type = user.user_type
+            if user_type == '1':
+                return redirect('hod_home')
+            elif user_type == '2':
+                return HttpResponse('This is Staff panel')
+            elif user_type == '3':
+                return HttpResponse('This is Student panel')
+            else:
+                messages.error(request,'Email and Password are Invalid')
+                return redirect('login')
+        else:
+            messages.error(request,'Email and Password are Invalid')
+            return redirect('login')
+
+
+def doLogout(request):
+    logout(request)
+    return redirect('login')
